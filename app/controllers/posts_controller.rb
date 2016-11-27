fc97d749a8a7d1271b9837bcf188e2b6257b0511
Chris Legg <chris@chrislegg.net>
2016-11-27 18:15:49 +0000

    Implement post policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    authorize @post
   end
 
   def update
+    authorize @post
     if @post.update(post_params)
       redirect_to @post, notice: 'You post was saved succesfull'
     else
