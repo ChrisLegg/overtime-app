c5849cea3455324c247246e224dbfcf104c87546
Chris Legg <chris@navondigital.com>
2016-12-16 15:45:22 +0000

    Add data to admin homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ puts "Admin User Created"
 
 100.times do |post| 
   Post.create(date: Date.today,
-              rationale: "#{post} rationale content",
+              rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
               user: @user,
               overtime_request: "2.5")
 end
